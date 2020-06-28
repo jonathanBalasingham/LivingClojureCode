1e200857d64bd2cbb37f5cdc69dbd29b9e98ca86
slaps LLC. Jonathan Balasingham <jonbalasin@gmail.com>
2020-06-28 13:03:50 -0700

    add next project

--- serpant-talk/src/serpant_talk/core.clj
+++
@@ -1,6 +0,0 @@
-(ns serpant-talk.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
--- serpant-talk/test/serpant_talk/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns serpant-talk.core-test
-  (:require [clojure.test :refer :all]
-            [serpant-talk.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
